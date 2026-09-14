c5ca5f1c10c75f793f83b423bf9388a25b2ab08c
Goonstabro <Goonstabro@gmail.com>
2026-09-14 18:03:44 -0400

    Creating the intial prototype of my break up plant app

---
+++ home.js
@@ -0,0 +1,9 @@
+const bodyPlantButton = document.getElementById("activity-button");
+const bodyActivitiesPanel = document.getElementById("activities-panel");
+
+
+
+
+bodyPlantButton.addEventListener("click", ()=>{
+bodyActivitiesPanel.classList.toggle("hidden");
+})
