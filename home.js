ad634f98052e9aae50f66409d32e021823981425
Goonstabro <Goonstabro@gmail.com>
2026-09-15 13:19:17 -0400

    Added the logging Activity interface ui, can't wait to keep adding features!

--- home.js
+++ home.js
@@ -1,9 +1,34 @@
 const bodyPlantButton = document.getElementById("activity-button");
 const bodyActivitiesPanel = document.getElementById("activities-panel");
+const bodyForm = document.getElementById("body-form");
+const bodyPlantActivitieButtons = document.querySelectorAll(".body-activities");
+const bodyMinutes = document.getElementById("body-minutes");
+const bodyResultMessage = document.getElementById("body-result");
 
 
+let selectedActivity = "";
 
 
 bodyPlantButton.addEventListener("click", ()=>{
-bodyActivitiesPanel.classList.toggle("hidden");
+    bodyActivitiesPanel.classList.toggle("hidden");
+})
+
+bodyPlantActivitieButtons.forEach((button) => {
+    button.addEventListener("click", ()=> {
+        selectedActivity = button.dataset.activity;
+    })
+})
+
+bodyForm.addEventListener("submit", (event)=> {
+    event.preventDefault();
+
+    if(!selectedActivity){
+        bodyResultMessage.innerHTML = "Select An Activity!"
+        return;
+    }
+
+   const currentMinutes = Number(bodyMinutes.value);
+
+   bodyResultMessage.innerHTML = `Completed ${currentMinutes} mins on the ${selectedActivity} activity!`;
+
 })
